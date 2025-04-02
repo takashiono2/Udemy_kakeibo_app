b000296961b0f63cfe3940b16792ad6293d33792
takashiono2 <takashiono710@gmail.com>
2025-04-02 23:51:48 +0900

    section5_27

--- src/components/Calender.tsx
+++ src/components/Calender.tsx
@@ -1,8 +1,44 @@
+import FullCalendar from '@fullcalendar/react'
 import React from 'react'
+import dayGridPlugin from '@fullcalendar/daygrid'
+import jaLocale from '@fullcalendar/core/locales/ja'
+import '../calender.css'
+import { EventContentArg } from '@fullcalendar/core'
 
 const Calender = () => {
+
+  const events = [
+    { title: 'Meeting', start: new Date(), income: 500, expese: 200, balance: 300 },
+    { title: 'Meeting', start: "2025-04-01", income: 300, expese: 200, balance: 100 }
+  ]
+
+  const renderEventContent = (eventInfo: EventContentArg) => {
+    return (
+      <div>
+        <div>
+          <div className="money" id="event-income">{eventInfo.event.extendedProps.income}</div>
+        </div>
+        <div>
+          <div className="money" id="event-expense">{eventInfo.event.extendedProps.expese}</div>
+        </div>
+        <div>
+          <div className="money" id="event-balance">{eventInfo.event.extendedProps.balance}</div>
+        </div>
+      </div>
+    )
+  }
+
+  //カスタムrenderEventContentの引数eventInfoの型も指定
+  //1度eventInfoをconsole.logで確認してみる
+  // returnでclass="money",id="event-incom", event-expense, event-balanceを指定
   return (
-    <div>Calender</div>
+    <FullCalendar
+      locale={jaLocale}
+      plugins={[dayGridPlugin]}
+      initialView="dayGridMonth"
+      events={events}
+      eventContent={renderEventContent}
+    />
   )
 }
 
